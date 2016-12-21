c532727e18191eb8ee68b5e2351d73a7e2f21777
swl <swl@swldeMacBook-Pro.local>
2016-12-21 11:15:20 -0800

    module_3 basic implementation

---
+++ module_3/app.js
@@ -0,0 +1,70 @@
+(function(){
+'use strict'
+
+angular.module("NarrowItDownApp",[])
+.controller("NarrowItDownController", NarrowItDownController)
+.service("MenuSearchService", MenuSearchService)
+.directive("foundItems", foundItemsDirective)
+.constant("ApiBaseUrl", "http://davids-restaurant.herokuapp.com");
+
+function foundItemsDirective() {
+    var ddo = {
+        templateUrl: 'items.html',
+        scope: {
+            items: '<',
+            onRemove: '&'
+        }
+    };
+    return ddo;
+}
+
+NarrowItDownController.$inject=['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+    var narrowItems = this;
+    narrowItems.found =[];
+    narrowItems.showMessage= false;
+
+    narrowItems.getNarrowedItems = function (){
+        narrowItems.found =[];
+        if (narrowItems.searchTerm){
+            var promise = MenuSearchService.getMatchedMenuItems(narrowItems.searchTerm);
+            promise.then(function(response) {
+                narrowItems.found = response;
+            })
+            .catch(function (error){
+                console.log("Something is wrong in the getNarrowedItems of NarrowItDownController")
+            })
+        }
+        if (!narrowItems.searchTerm || narrowItems.found.length==0) {
+            narrowItems.showMessage = true;
+        }
+    };
+
+    narrowItems.removeNarrowedItems = function (itemIdx){
+        narrowItems.found.splice(itemIdx,1);
+    };
+
+}
+
+MenuSearchService.$inject=['$http','ApiBaseUrl']
+function MenuSearchService($http, ApiBaseUrl) {
+    var service = this;
+    service.getMatchedMenuItems = function(searchTerm) {
+        return $http({
+            url: (ApiBaseUrl+ "/menu_items.json")
+        }).then(function(result){
+            var foundItems =[];
+            var items = result.data.menu_items;
+            for (var i=0; i<items.length; i++){
+                if (searchTerm!= '' && items[i].description.indexOf(searchTerm)!= -1)
+                    foundItems.push(items[i]);
+            }
+
+            for (var i=0; i<foundItems.length; i++)
+                console.log(foundItems[i]);
+            return foundItems;
+        });
+    }
+
+}
+})()
